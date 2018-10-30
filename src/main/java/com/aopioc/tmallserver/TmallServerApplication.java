e9aea5b0abd3bc7b5b360841f5ad3cd1ae8f7eb8
JefferyChang <cglcoder@sina.com>
2018-10-30 23:58:11 +0800

    init

---
+++ src/main/java/com/aopioc/tmallserver/TmallServerApplication.java
@@ -0,0 +1,12 @@
+package com.aopioc.tmallserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmallServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmallServerApplication.class, args);
+	}
+}
